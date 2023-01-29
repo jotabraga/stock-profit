5a81f0fbfdd6294c6b85a0929b5f2ac09240a302
jotabraga <jpmbraga.feg@gmail.com>
2023-01-29 20:14:52 -0300

    feat: add comments

--- profitCalc.js
+++ profitCalc.js
@@ -1,18 +1,21 @@
 function calcProfit(prices) {
+  //assign the min price as the first element to compare with the following ones
   let minPrice = prices[0];
   let maxProfit = 0;
 
   prices.forEach((price) => {
-    const currentTransactionProfit = Math.max(maxProfit, price - minPrice);
+    //compares the max profit saved with the difference between the current day price and the min price got until the moment
+    const currentMaxTransactionProfit = Math.max(maxProfit, price - minPrice);
+    //if the day price is lower than minprice minprice receives the price
     if (price < minPrice) {
       minPrice = price;
     }
-    maxProfit = currentTransactionProfit;
+    maxProfit = currentMaxTransactionProfit;
   });
 
   return maxProfit;
 }
 
-console.log(calcProfit([7, 1, 5, 3, 6, 4]));
-console.log(calcProfit([7, 6, 4, 3, 1]));
-console.log(calcProfit([]));
+console.log(calcProfit([7, 1, 5, 3, 6, 4])); //output 5
+console.log(calcProfit([7, 6, 4, 3, 1])); //output 0
+console.log(calcProfit([])); //output 0
